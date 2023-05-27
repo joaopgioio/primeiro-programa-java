bd7f405d588302b244d5a2c8730cf0183d030482
João Paulo <jpalves@minsait.com>
2023-05-27 10:22:33 -0300

    Adicionado texto

--- primeiro-programa-java/src/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/executavel/PrimeiraClasseJava.java
@@ -3,7 +3,7 @@ package executavel;
 public class PrimeiraClasseJava {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!!!!!!");
+		System.out.println("Hello World, João Paulo!!!!!!");
 	}
 
 }
