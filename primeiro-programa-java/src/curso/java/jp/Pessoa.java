91b5a2162df6694e179caeb661a2fac5539ff3d0
João Paulo <jpalves@minsait.com>
2023-05-27 10:58:41 -0300

    Incluido o atributo data nascimento

--- primeiro-programa-java/src/curso/java/jp/Pessoa.java
+++ primeiro-programa-java/src/curso/java/jp/Pessoa.java
@@ -1,11 +1,28 @@
 package curso.java.jp;
 
+import java.util.Date;
+
 public class Pessoa {
 	
 	private String nome;
 	private String sobrenome;
+	private int idade;
+	private Date dataNascimento;
+	
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
 	public String getNome() {
 		return nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
