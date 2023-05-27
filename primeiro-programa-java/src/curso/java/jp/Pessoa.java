e1f25a4bf762458cc637aefb34d5f63b4757e347
João Paulo <jpalves@minsait.com>
2023-05-27 10:58:41 -0300

    Adicionando o campo idade a classe pessoa

--- primeiro-programa-java/src/curso/java/jp/Pessoa.java
+++ primeiro-programa-java/src/curso/java/jp/Pessoa.java
@@ -4,8 +4,16 @@ public class Pessoa {
 	
 	private String nome;
 	private String sobrenome;
+	private int idade;
+	
 	public String getNome() {
 		return nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
