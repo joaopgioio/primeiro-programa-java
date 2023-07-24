ca02614cb149e216ad78222cedde08edb3a8c134
João Paulo <jpalves@minsait.com>
2023-07-24 19:53:13 -0300

    subindo datanascimento2

--- primeiro-programa-java/src/curso/java/jp/Pessoa.java
+++ primeiro-programa-java/src/curso/java/jp/Pessoa.java
@@ -8,6 +8,7 @@ public class Pessoa {
 	private String sobrenome;
 	private int idade;
 	private Date dataNascimento;
+	private Date dataNascimento2;
 	
 	public Date getDataNascimento() {
 		return dataNascimento;
